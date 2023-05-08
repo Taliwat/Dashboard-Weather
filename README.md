b3322f14a26e6e0696264314ecc8bdf796ef06e6
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-08 15:05:09 -0400

    removed unnec comments and added readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Weather Dashboard
+
+## Description
+-a useful application that allows the user to look up the current weather forecast for any city and get current results as well as a 5-day forecast.
+
+## Table of Contents
+
+- [Usage]
+- [Credits]
+
+## Usage
+
+-Click on the search tab at the top and enter a city.  It then shows you your current results in the form of your conditions as well as your extended forecast.
+
+## Credits
+Ryan Mennemeier
+
+## Contact
+Email: kalpha1865@gmail.com
+Github: @Taliwat
--- script.js
+++ script.js
@@ -47,41 +47,11 @@ function getAPIdata(city) {
 searchBtn.addEventListener("click", function () {
     let value = searchBar.value
     getAPIdata(value)
-})
+});
 
     
     
       
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-//note!  will need to use lat and lon as coordinates for the city in the function
-
-
-
-
-
-
-
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-
-
-
-
-
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-
-
-
-
-
-
-
-//WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city
 
 
 
